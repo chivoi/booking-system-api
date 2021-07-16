a7c5a34c5a07479ec1c77790547290c31f0260d3
chivoi <a.lastovirya@gmail.com>
2021-07-16 20:15:56 +1000

    add CRUD functionality to bookings, timeslot blocks/releases with create and delete, test with client.http

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,7 @@
 class BookingsController < ApplicationController
 
+  before_action :set_booking, only: [:show, :update, :destroy]
+
   def user_bookings 
     @bookings = current_user.bookings
     render json: @bookings
@@ -10,5 +12,47 @@ class BookingsController < ApplicationController
     render json: @all_bookings
   end
 
+  def create 
+    @booking = Booking.create(booking_params)
+    if @booking.errors.any?
+      render json: @booking.errors, status: :unprocessable_entity
+    else
+      @booking.timeslot.update(is_blocked: true)
+      render json: @booking, status: 201
+    end
+  end
+
+  def update
+    @booking.update(booking_params)
+    if @booking.errors.any?
+      render json: @booking.errors, status: :unprocessable_entity
+    else 
+      render json: @booking, status: 201
+    end
+  end
+
+  def show
+    render json: @booking
+  end
+
+  def destroy 
+    @booking.timeslot.update(is_blocked: false)
+    @booking.destroy
+    render json: 204
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:timeslot_id, :user_id, :venue, :address, :event_type, :start_time, :duration, :pa_provided, :message)
+  end
+
+  def set_booking
+    begin
+      @booking = Booking.find(params[:id])
+    rescue
+      render json: {"error": "Booking not found"}, status: 404
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   scope '/api' do
     get '/my_bookings', to: 'bookings#user_bookings'
     get '/bookings', to: 'bookings#all_bookings'
+    post '/bookings', to: 'bookings#create'
+    get '/bookings/:id', to: 'bookings#show'
+    patch '/bookings/:id', to: 'bookings#update'
+    delete '/bookings/:id', to: 'bookings#destroy'
     scope '/auth' do
       post '/sign_up', to: 'users#create'
       post '/log_in', to: 'users#log_in'
