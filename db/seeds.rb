f575aa678a4bd956a8108f5bdb8853b9c41c21d0
chivoi <a.lastovirya@gmail.com>
2021-07-28 18:49:23 +1000

    edit users in the seeds file to be more generic

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ end
 
 # seeding users
 if User.all.length == 0
-  User.create(first_name:"Ana", last_name: "Lastoviria", phone_num: "0404040404", email: "ana@test.com", password:"password", password_confirmation:"password")
-  User.create(first_name:"Alex", last_name: "Velasco", phone_num: "123456789", email: "alex@test.com", password:"password1", password_confirmation:"password1", is_admin: "true")
+  User.create(first_name:"Regular", last_name: "User", phone_num: "0404040404", email: "user@test.com", password:"iamuser", password_confirmation:"iamuser")
+  User.create(first_name:"Admin", last_name: "Admin", phone_num: "123456789", email: "admin@test.com", password:"iamadmin", password_confirmation:"iamadmin", is_admin: true)
   puts "Created 1 x user + 1 x admin"
 end
